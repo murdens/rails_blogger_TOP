bd8372c9291521205550e6d84d2ff6a627634ee4
murdens <sonya.murden@gmail.com>
2020-02-28 22:55:42 +0000

    added tagging to blog

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
     resources :articles do
       resources :comments
     end
+    root to: 'tags#index'
+    resources :tags
+
   end
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
