bccf8cb408f71f657c78072037d42e6604650e9f
murdens <sonya.murden@gmail.com>
2020-03-01 16:48:49 +0000

    blog authors added and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,12 @@ Rails.application.routes.draw do
     root to: 'tags#index'
     resources :tags
 
+    resources :authors
+    resources :author_sessions, only: [ :new, :create, :destroy ]
+
+get 'login'  => 'author_sessions#new'
+get 'logout' => 'author_sessions#destroy'
+
   end
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
